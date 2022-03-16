40c1804274a494279fd918407622c87a3fd3237b
Evgeny0088 <evgeny.kuznetsov0088@gmail.com>
2022-03-16 15:13:33 +0300

    api gateway service is created, later on all requests will pass through that one

---
+++ APIgateway/src/main/java/com/marketplace/APIgateway/ApIgatewayApplication.java
@@ -0,0 +1,13 @@
+package com.marketplace.APIgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class ApIgatewayApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ApIgatewayApplication.class, args);
+	}
+}
