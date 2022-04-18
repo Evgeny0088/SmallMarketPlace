cacba36a7abc6a1a7748d5d7a47adcab93d749b2
Evgeny Kuznetsov <evgeny.kuznetsov0088@gmail.com>
2022-04-18 13:41:19 +0300

    sh files are updated

---
+++ docker/app_run.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+UPLOAD='\U0001f680';
+LAUGH='\U0001f602';
+DOCKER='\U0001f433';
+DOCKER_HUB='evgeny88docker';
+
+declare -a images=("${DOCKER_HUB}/registry-service:1.0"
+                   "${DOCKER_HUB}/config-service:1.0"
+                   "${DOCKER_HUB}/gateway-service:1.0"
+                   "${DOCKER_HUB}/itemstorage-service:1.0"
+                   "${DOCKER_HUB}/saleorders-service:1.0")
+
+echo -e "hi, welcome to small market place!...${LAUGH}\n"
+sleep 1
+
+echo -e "Starting to load service images from repositoty...${UPLOAD}\n"
+
+for image in "${images[@]}";
+    do
+      if ! docker pull ${image}; then
+         echo -e "${image} image not found >>> please create new image!...\n"
+         set -e
+      else
+         echo -e "image found!... ${DOCKER}\n"
+      fi
+    done
+
+docker-compose -f servers.yml -f services.yml up --no-build &&\
+docker-compose -f services.yml down --rmi local &&\
+docker-compose -f servers.yml down --rmi local
+
+
--- small_market_place_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-LAUGH='\U0001f602';
-echo -e "hi, welcome to small market place${LAUGH}"
-
-docker-compose -f servers.yml -f services.yml up &&\
-docker-compose services.yml down --rmi local &&\
-docker-compose servers.yml down --rmi local
-
-
