c1a3c7531185d535697de1cc58b89c7355135e25
Evgeny Kuznetsov <evgeny.kuznetsov0088@gmail.com>
2022-04-28 16:18:58 +0300

    configs are setup for docker and minikube

--- k8s_deploy/services/services_deploy.sh
+++ k8s_deploy/services/services_deploy.sh
@@ -1,1 +1,9 @@
+REDIS_HOSTNAME=$(kubectl exec -i -t redis-primary-0 -- hostname -i)
+
+itemstorage_redis_host=`cat "itemstorage-service_deploy.yml" | sed "s/{{REDIS_HOSTNAME}}/$REDIS_HOSTNAME/g"`
+saleorders_redis_host=`cat "saleorders-service.yml" | sed "s/{{REDIS_HOSTNAME}}/$REDIS_HOSTNAME/g"`
+
 kubectl apply -f ./
+
+echo "$itemstorage_redis_host" | kubectl apply -f -
+echo "$saleorders_redis_host" | kubectl apply -f -
